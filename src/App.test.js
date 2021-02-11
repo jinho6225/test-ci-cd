0704926e2c8c7706f6ddc56031cff0ab686a1d68
jinho6225 <jinho6225@hotmail.com>
2021-02-10 20:53:58 -0800

    testing code update

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,18 @@ const puppeteer = require("puppeteer");
 
 describe("Puppeteer test", () => {
   it("it is testing for Title", async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("http://localhost:3000/test-ci-cd");
     const title = await page.$eval(".todo_list", (e) => e.innerHTML);
     expect(title).toBe("Todo List");
     browser.close();
   });
 
   it("it is testing for count of li tag", async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("http://localhost:3000/test-ci-cd");
     const liArray = await page.evaluate(() => {
       return document.querySelectorAll("li").length;
     });

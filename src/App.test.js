0938031dab5a20fb6ceb565e6e9ca8bb9d2580f7
jinho6225 <jinho6225@hotmail.com>
2021-02-10 20:41:47 -0800

    adjust yml file

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ describe("Puppeteer test", () => {
   it("it is testing for Title", async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("https://jinho6225.github.io/test-ci-cd/");
     const title = await page.$eval(".todo_list", (e) => e.innerHTML);
     expect(title).toBe("Todo List");
     browser.close();
@@ -13,7 +13,7 @@ describe("Puppeteer test", () => {
   it("it is testing for count of li tag", async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("https://jinho6225.github.io/test-ci-cd/");
     const liArray = await page.evaluate(() => {
       return document.querySelectorAll("li").length;
     });
